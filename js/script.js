86e53fd9ae5b70fd09f5a666171688ee7e32f0ec
lelz99 <emanuelesimonetti99@gmail.com>
2024-01-23 18:17:43 +0100

    created a button to add new tasks

--- js/script.js
+++ js/script.js
@@ -3,15 +3,26 @@ const app = createApp({
     data() {
         return{
             tasks,
+            newTaskText: ''
         }
     },
     methods: {
         deleteTask(id){
             const newTasks = this.tasks.filter( task => id !== task.id)
             this.tasks = newTasks
+        },
+        addNewTask(){
+            const newTask = {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTaskText,
+            }
+
+            this.tasks.push(newTask)
+
+            this.newTaskText = ''
         }
     }
 });
 
 app.mount('#root')
-console.log(tasks)
